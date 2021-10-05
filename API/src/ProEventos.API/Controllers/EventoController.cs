339d5892ba4ade9191ace8d14cf63ff4a11e87f7
LuisCurado <luisaugusto.trindadecurado@outlook.com>
2021-10-05 17:46:56 -0300

    Curso .net 5 e Angular 11 inicio

---
+++ API/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {        
+        public EventoController()
+        {            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento []
+            {
+                new Evento {
+                    EventoId = 1, 
+                    Tema = "Angular 11 e .Net 5", 
+                    Local = "Belo Horizonte", 
+                    Lote = "1º Lote", 
+                    QauntidadePessoas = "250", 
+                    DataEvento = DateTime.Now.add(2).ToString(),
+                    ImagemURL = "foto.png"
+                },                
+                new Evento {
+                    EventoId = 1, 
+                    Tema = "Angular 11 e .Net 5", 
+                    Local = "Belo Horizonte", 
+                    Lote = "1º Lote", 
+                    QauntidadePessoas = "250", 
+                    DataEvento = DateTime.Now.add(2).ToString(),
+                    ImagemURL = "foto.png"
+                },
+            };
+        }
+    }
+}
